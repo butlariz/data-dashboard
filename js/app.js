3f743175d404ca5774e72f2c774c64d3eedce6b6
butlariz <laris.santana09@gmail.com>
2018-07-14 18:48:35 -0300

    função e grafico com array

--- js/app.js
+++ js/app.js
@@ -1,12 +1,11 @@
-
 var sede = 'AQP';
 var turma = '2016-2';
-// totalAndInactives();
-var inactivesPercent = 0;
-var activesPercent = 0;
+var studentsTotal = totalAndInactives();
 
-// Contar o total de alunas e alunas inativas
-// function totalAndInactives(){
+// Contar o total de alunas ativas e inativas
+function totalAndInactives(){
+  var activesPercent = 0;
+  var inactivesPercent = 0;
   var inactives = 0;
 	var totalStudents = data[sede][turma]['students'].length;
 	for (i in data[sede][turma]['students']){
@@ -16,41 +15,31 @@ var activesPercent = 0;
   }
   inactivesPercent += (inactives/totalStudents * 100);
   activesPercent += 100% - inactivesPercent;
-// }
+  var total = [activesPercent,inactivesPercent]
+  return total;
+}
 
-console.log(inactivesPercent + "" + activesPercent)
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+  function drawChart() {
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Status');
+    data.addColumn('number', 'Porcentagem');
+    data.addRows([
+      ['Ativas', studentsTotal[0]],
+      ['Inativas', studentsTotal[1]]
+    ]);
 
-  google.charts.load('current', {'packages':['corechart']});
-  google.charts.setOnLoadCallback(drawChart);
-    function drawChart() {
-      var data = new google.visualization.DataTable();
-      data.addColumn('string', 'Status');
-      data.addColumn('number', 'Porcentagem');
-      data.addRows([
-        ['Ativas', activesPercent],
-        ['Inativas',inactivesPercent]
-      ]);
+    var options = {'title':'Total e Desistencia',
+                    'width':400,
+                    'height':300};
 
-      var options = {'title':'Total e Desistencia',
-                      'width':400,
-                      'height':300};
-
-      var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-      chart.draw(data, options);
-  }
-
-
+    var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
+}
 
 
 
-
-// console.log(totalStudents);
-// 
-// console.log(inactivesPercent);
-// console.log(data[sede]['2016-2']['students']);
-// console.log(data[sede]['2016-2']['students'].length);
 // console.log(data[sede][turma]['students'][i]['name'] + "-" + data[sede]['2016-2']['students'][i]['active']);
-
-// img.src = data[sede][turma]['students'][i]['photo'];
 // listaProgramadoras.appendChild(img);
 // var listaProgramadoras = document.getElementById('exibe-programadoras');
