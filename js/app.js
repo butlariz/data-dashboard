be7ce26df6c2906ceda9468abdf77db59e855c36
Ana Oliveira <ana_oli@outlook.com>
2018-07-15 14:05:50 -0300

    teste grafico nota tech

--- js/app.js
+++ js/app.js
@@ -287,11 +287,37 @@ totalStudents = data[sede][turma]['students'].length
         } else if (!sprintsHitGoal[j]){
           sprintsHitGoal[j] = 1;
           percentHitGoal[j] = (1 / totalStudents * 100);
+          var scoreTotal = [percentHitGoal, sprintsHitGoal]
+          techGraph(scoreTotal);
+          console.log(scoreTotal);
+         }
+     function techGraph(value) {
+   google.charts.load('current', {'packages':['corechart']});
+   google.charts.setOnLoadCallback(function(){
+     drawChart(value);
+   });
+   function drawChart(valueGraphTech) {
+     var data = new google.visualization.DataTable();
+     data.addColumn('string', 'Status');
+     data.addColumn('number', 'Porcentagem');
+     data.addRows([
+       ['Sprint1', valueGraphTech[0]],
+       ['Sprint2', valueGraphTech[1]],
+     ]);
+     var options = {'title':'Notas e excedem',
+                     'width':400,
+                     'height':300};
+ 
+     var chart = new google.visualization.PieChart(document.getElementById('chart_div1'));
+     chart.draw(data, options);
+          }
         }
       }
     }
   }
 }
+
+
 
 //console.log(data[sede][turma]['students'][i]['name'] + ":" + data[sede][turma]['students'][i]['sprints'][0]['score']['tech'])
 // console.log(data[sede][turma]['students'][i]['name'] + "-" + data[sede]['2016-2']['students'][i]['active']);
